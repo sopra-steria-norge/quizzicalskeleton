5d8cc6161b324c9753b7e05ecc8ec23e1b7facc8
Ørjan Markhus Lillevik <olillevik@gmail.com>
2013-06-23 12:42:43 +0200

    .gitignore added

---
+++ target/quizzical-1.0/scripts/quizCtrl.js
@@ -0,0 +1,15 @@
+function QuizCtrl($scope,$http) {
+	$scope.numberOne = 3;
+	$scope.numberTwo = 2;
+
+	$scope.addNumbers = function() {
+		$scope.sum = $scope.numberOne + $scope.numberTwo;
+		$http({method: "POST",url: "quiz/sum", data: {one: $scope.numberOne, two: $scope.numberTwo}}).
+		success(function(data) {
+			$scope.serversum = data.sum;
+		}).
+		error(function(data,status) {
+			console.log("Error:" + status);
+		});
+	};
+}
