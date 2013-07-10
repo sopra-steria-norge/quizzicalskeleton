aadce3da177201a8eb406b244034f74ea874928c
nikolahy <nhyldmo@NOLSOMMERV14.one.steria.dom>
2013-07-10 10:21:05 +0200

    Changed colors radio buttons and top navbar color and shadow

--- src/main/webapp/prototype/quiz.js
+++ src/main/webapp/prototype/quiz.js
@@ -7,20 +7,11 @@ function QuizCtrl($scope){
 		var labels = document.getElementsByClassName("radio quiz-input-choice-box");
 		
 		for (var i = 0; i < element.length; i++) {
-			//alert(labels[i].style.background);
-			//labels[i].style.background = "#fff";
-			
 			//remove the class quiz-input-choice-box-selected if found
-			labels[i].className = 
-				labels[i].className.replace( /(?:^|\s)quiz-input-choice-box-selected(?!\S)/g , '' );
+			labels[i].className = labels[i].className.replace( /(?:^|\s)quiz-input-choice-box-selected(?!\S)/g , '' );
 			
 			if (element[i].checked == true) {
 				labels[i].className += " quiz-input-choice-box-selected";
-				
-				//var newColor = "#aaa"//element[i].value;
-				//document.getElementById("changeColor").style.background = newColor;
-				//element[i].getParent().style.background = "#888";
-				//labels[i].style.background = "#888";
 			}
 		}
 	};
