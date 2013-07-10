473b0e33ad3edef347acf42723f9d27b9f3d70d9
Anders Karlsen <karlsenanders@gmail.com>
2013-07-10 13:45:52 +0200

    Controller test

--- src/test/javascript/quizControlSpec.js
+++ src/test/javascript/quizControlSpec.js
@@ -4,9 +4,21 @@ describe("Quiz Cotroller", function() {
 
     beforeEach(inject(function($controller, $rootScope, $injector) {
         scope = $rootScope;
+        var mockServer = {};
+        mockServer.success = function(successFunction) {
+        	successFunction({sum: 42});
+        	return mockServer;
+        };
+        mockServer.error = function(errorFunction) {
+        	return mockServer;
+        };
+        var mockhttp = function(config) {
+        	return mockServer;
+        };
 
         controller = $controller('QuizCtrl', {
-            $scope : scope
+            $scope : scope,
+            $http: mockhttp
         });
 
     }));
@@ -14,5 +26,12 @@ describe("Quiz Cotroller", function() {
   it("has default values", function() {
     expect(scope.numberOne).toEqual(3);
     expect(scope.numberTwo).toEqual(2);
+  });
+
+  it("computes sum", function() {
+  	scope.numberOne = 4;
+  	scope.addNumbers();
+  	expect(scope.sum).toEqual(6);
+  	expect(scope.serversum).toEqual(42);
   });
 });
