fdc278ec91b624fde6359786aa9a0a36fbc2beae
Anders Karlsen <karlsenanders@gmail.com>
2013-07-10 13:23:34 +0200

    Basic testing of quiz controller

--- src/test/javascript/quizControlSpec.js
+++ src/test/javascript/quizControlSpec.js
@@ -1,5 +1,18 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+describe("Quiz Cotroller", function() {
+    var controller;
+    var scope;
+
+    beforeEach(inject(function($controller, $rootScope, $injector) {
+        scope = $rootScope;
+
+        controller = $controller('QuizCtrl', {
+            $scope : scope
+        });
+
+    }));
+
+  it("has default values", function() {
+    expect(scope.numberOne).toEqual(3);
+    expect(scope.numberTwo).toEqual(2);
   });
 });
