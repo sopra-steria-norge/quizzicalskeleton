3ae31d150f8eda7db3d86768ac254b7eedef699e
nikolahy <nhyldmo@NOLSOMMERV14.one.steria.dom>
2013-07-22 17:53:16 +0200

    Included some javascript libraries, further work on adminaddquiz

--- src/main/webapp/scripts/adminCtrl.js
+++ src/main/webapp/scripts/adminCtrl.js
@@ -1,5 +1,15 @@
 angular.module('quizControllers')
 .controller('AdminCtrl', ['$scope', '$http',
     function($scope, $http) {
+		$scope.newquiz = {
+				quizName: "",
+				quizDesc: "",
+				submitMsg: "",
+				questions: [{id: 1, text: "", alternatives: [{aid:1, atext: "Aaa"}], answer: 0}]
+		};
 		
+		$scope.addAlternative = function(question){
+			question.alternatives.push({aid:2, atext: ""});
+		};
 }]);
+
