bb412804ad59630bf0f05c98f3feb414c8534a9a
Anders Karlsen <karlsenanders@gmail.com>
2013-07-29 15:24:41 +0200

    Added PasswordUtil

---
+++ src/test/java/no/steria/quizzical/PasswordUtilTest.java
@@ -0,0 +1,18 @@
+package no.steria.quizzical;
+
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class PasswordUtilTest {
+	@Test
+	public void shouldTestPassword() throws Exception {
+		PasswordUtil passwordUtil = new PasswordUtil();
+		byte[] salt = passwordUtil.generateSalt();
+		byte[] crypt = passwordUtil.getEncryptedPassword("secret", salt);
+
+		assertThat(passwordUtil.authenticate("secret", crypt, salt)).isTrue();
+		assertThat(passwordUtil.authenticate("false", crypt, salt)).isFalse();
+	}
+}
